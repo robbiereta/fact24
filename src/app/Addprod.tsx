ce865d8dabcbae26e286b850505942c2e927e0f4
robbie reta <robbie.ret67@gmail.com>
2024-02-15 13:44:51 -0600

    .

---
+++ src/app/Addprod.tsx
@@ -0,0 +1,59 @@
+'use client'
+function Addprod() {
+    
+    let  notasPartidas: {
+        clave_producto_servicio: string; clave_unidad_de_medida: string; cantidad: number; descripcion: string; valor_unitario: number; total // will use psql environment variables
+          : number; exento_de_impuestos: boolean; impuestos: { traslados: { traslado: { base: any; impuesto: string; tipoFactor: string; tasaOCuota: number; importe: number; tipo: string; }[]; }; };
+      }[]=  [];
+      
+    function addPartida(importe: any) {
+     
+    
+      let iva = Number(importe) * 0.16
+      
+      
+        let partida= {
+         
+          clave_producto_servicio: "01010101",
+          clave_unidad_de_medida: "ACT",
+          cantidad: 1,
+          descripcion: "Venta",
+          valor_unitario: Number(importe),
+          total: Number(importe),
+          impuestos: {
+            traslados: {
+            traslado: [
+            {
+            base: importe,
+            impuesto: "002",
+            tipoFactor: "Tasa",
+            tasaOCuota: 0.16,
+            importe: Number(importe) - iva,
+            tipo: "Traslado"
+            }
+            ]
+            }
+         }
+         
+        }
+        notasPartidas.push(partida)
+      
+      console.log(notasPartidas);
+      }
+      function onSubmit(formData: FormData) {
+        const importe = formData.get("importe");
+        console.log(importe);
+        addPartida(importe)
+        
+      }
+    return (
+      <form action={onSubmit}>
+        <input name="importe" />
+        <button type="submit">Agregar</button>
+      </form>
+    );
+  
+  }
+  
+  export default Addprod
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,122 @@
 import Image from "next/image";
-
 import postgres from 'postgres'
+import { useForm } from "react-hook-form";
+import Addprod from "./Addprod";
 
 const sql = postgres( 'postgres://robbie:1234@localhost:5432/mydb ') // will use psql environment variables
-
-async function getNotes() {
-  const notes = await sql`
-  SELECT id,partner_id, fiscal_position_id, payment_term_id, pricelist_id, create_uid, write_uid, name, state, client_order_ref, origin, reference, signed_by, invoice_status, validity_date, note, currency_rate, amount_untaxed, amount_tax, amount_total, amount_to_invoice, locked, require_signature, require_payment, create_date, commitment_date, date_order, signed_on, write_date, prepayment_percent, sale_order_template_id
-	FROM public.sale_order WHERE partner_id = '44';
-  `
-  // notes = Result [{ name: "Walter", age: 80 }, { name: 'Murray', age: 68 }, ...]
-  return notes
-}
+    async function getNotes() {
+      let notes = await sql`SELECT id,partner_id, fiscal_position_id, payment_term_id, pricelist_id, create_uid, write_uid, name, state, client_order_ref, origin, reference, signed_by, invoice_status, validity_date, note, currency_rate, amount_untaxed, amount_tax, amount_total, amount_to_invoice, locked, require_signature, require_payment, create_date, commitment_date, date_order, signed_on, write_date, prepayment_percent, sale_order_template_id
+      FROM public.sale_order WHERE partner_id = '44';
+      `
+      // notes = Result [{ name: "Walter", age: 80 }, { name: 'Murray', age: 68 }, ...]
+      return notes
+    }
+    
+   
 
 let notes = getNotes()
+let  notasPartidas: {
+  clave_producto_servicio: string; clave_unidad_de_medida: string; cantidad: number; descripcion: string; valor_unitario: number; total // will use psql environment variables
+    : number; exento_de_impuestos: boolean; impuestos: { traslados: { traslado: { base: any; impuesto: string; tipoFactor: string; tasaOCuota: number; importe: number; tipo: string; }[]; }; };
+}[]=  [];
+
+function addPartida(notaManual: any) {
+     
+  const importe =  notaManual.amount_total
+let folio = notaManual.name
+let iva = Number(importe) * 0.16
+
+
+  let partida= {
+   
+    clave_producto_servicio: "01010101",
+    clave_unidad_de_medida: "ACT",
+    cantidad: 1,
+    descripcion: "Venta",
+    valor_unitario: Number(importe),
+    total: Number(importe),
+    exento_de_impuestos: false,
+    impuestos: {
+      traslados: {
+      traslado: [
+      {
+      base: importe,
+      impuesto: "002",
+      tipoFactor: "Tasa",
+      tasaOCuota: 0.16,
+      importe: Number(importe) - iva,
+      tipo: "Traslado"
+      }
+      ]
+      }
+   }
+   
+  }
+  notasPartidas.push(partida)
+
+console.log(notasPartidas);
+}
 
 notes.then((notes) => notes.map((note) => {
-  let sec =  Date.now()+21600000;
+  let sec =  Date.now();
   let hoy = new Date(sec).toISOString().split('T')[0];
 
   let fechaCreacion = new Date(note.create_date).toISOString().split('T')[0];
- 
    
+  console.log(hoy,note.name,fechaCreacion,
+    note.amount_untaxed,note.amount_total,note.invoice_status,note.state);
+  
   // namecheap pone la fecha de creacion 12 horas despues
   //console.log(note);
   
-  if (note.partner_id == '44' && fechaCreacion== hoy && note.invoice_status == 'invoiced') {
+  if (note.partner_id == '44' && fechaCreacion== hoy && note.invoice_status == 'to invoice') {
 
-  let total =  note.amount_untaxed
-let folio = note.name
-  console.log(total,folio)
+
+    addPartida(note);
+}
+console.log(notasPartidas);
+
+}
+
+)
+
+)
+
+
+ let fact = {
+  "factura": {
+    "version": "string",
+    "fecha": "2019-08-24T14:15:22Z",
+    "tipo": "ingreso",
+    "Exportacion": "string",
+    "generacion_automatica": true,
+    "subtotal": null,
+    "descuento": null,
+    "impuesto_federal": null,
+    objeto_imp: "01",
+    "moneda": "MXN",
+    "total": null,
+    conceptos:notasPartidas,
+    "serie": "string",
+    "folio": null,
+    "InformacionGlobal": {},
+    "CfdiRelacionados": [],
+    "Complemento": {},
+    "pagos": [],
+    "send_pdf_and_xml_by_mail": true,
+    "emails_send": "string"
   }
-}))
+ }
 
 
-export default function Home() {
+export  default async  function Home() {
+  'use server'
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <main className="flex min-h-screen flex-col items-center p-24">
+    <div>   
+      <Addprod />
+    </div>
+  </main>
   );
 }
