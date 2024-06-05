cbf2ed52d5750288c8a11c3ad09276a92280bee1
Robbie Reta <robbie.reta67@gmail.com>
2024-06-05 11:59:13 -0600

    .

--- src/app/uiComponents/formCreator.tsx
+++ src/app/uiComponents/formCreator.tsx
@@ -2,6 +2,9 @@
 import Form from 'react-bootstrap/Form';
 import axios from "axios";
 import { FormGroup, Button } from 'react-bootstrap';
+import React from 'react';
+
+import AsyncSelect from 'react-select/async';
 
 interface FormCreatorProps {
   elements: {
@@ -17,39 +20,68 @@ interface FormCreatorProps {
 
 function FormCreator(props:FormCreatorProps) {
 
-  function setBlankForm() {
   let elements=props.elements
   let formElements:any[]=[]
-  elements.map((element)=>{
-    formElements.push(<FormGroup controlId={element.name+element.id}>
-      <Form.Label>{element.name}</Form.Label>
-      <Form.Control type={element.type} placeholder={element.placeholder} name={element.name} />  
-    </FormGroup>)
+  var optionelements:any[]=[]
   
-})
 
+  let obj:any
+  function getRequest(url:string) {
+    axios.get(url)
+    .then(function (response) {
+         obj= response.data
+      
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    return obj
+   }
+
+ 
+    
+   elements.map((element)=>{
+  formElements.push(
+    <Form.Group className="mb-3" controlId={element.name+element.id}>
+    <Form.Label>{element.name}</Form.Label>
+    <Form.Control type="text" name={element.name} placeholder={element.placeholder} />
+  </Form.Group>
+  )
   return formElements
-  } 
-  
+  })
 
-  let formElements = setBlankForm();
+ 
+
+   function postRequest(url:string,data:any) {
+    axios.post(url,data)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+   }
   
 
   function onSubmit(formData: FormData) {
     let entries = Object.fromEntries(formData.entries()); 
     console.log(entries);
-    axios.post(props.recurso, entries)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-      formElements = setBlankForm();
+  postRequest(props.recurso,entries) 
+  
   }
-    return (
+  
+  var opt=getRequest("https://express-low5.onrender.com"+"/productos")  
+  return (
     <Form action={onSubmit}>
       {formElements}
+      <Form.Group className="mb-3" controlId="producto1">
+      <Form.Label>p</Form.Label>
+      <select name="producto" id="producto" >{opt.map((option)=>
+        (
+          <option key={option}>{option}</option>
+        ))}
+      </select>
+        </Form.Group> 
       <Button variant="primary" type="submit" >
         Guardar
       </Button>
