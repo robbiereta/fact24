147cb8bda01dce968d4ae03238f5bcb5c262899e
Robbie Reta <robbie.reta67@gmail.com>
2025-01-09 12:23:10 -0600

    .

--- src/app/libComponents/swConector.tsx
+++ src/app/libComponents/swConector.tsx
@@ -6,31 +6,25 @@ import { requireNoAuth } from '@/lib/auth';
 
 requireNoAuth();
 function swConnector(fact:object) {
-    
-
-
-function sendFactura(){
-let config = {
-	method: 'post',
-	maxBodyLength: Infinity,
-	url: 'http://services.sw.com.mx/v3/cfdi33/issue/json/v4',
-	headers: { 
-	  'Authorization': 'Bearer '+process.env.tokenTest,
-	  'Content-Type': 'application/jsontoxml'
-	},
-	data : fact
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://services.sw.com.mx/v3/cfdi33/issue/json/v4',
+    headers: { 
+      'Authorization': 'Bearer '+process.env.tokenTest,
+      'Content-Type': 'application/jsontoxml'
+    },
+    data: fact
   };
   
-  axios.request(config)
-  .then((response) => {
-   return response
-})
-  .catch((error) => {
-	console.log(error);
-  });
-  
-}
-sendFactura()
+  return axios.request(config)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
 }
 
 export default swConnector
